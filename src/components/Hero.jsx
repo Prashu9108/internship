56f8cc23d18b8fb6e33ba94f24ba0f7f9dd36739
Prashu9108 <prashu9108@gmail.com>
2023-12-22 16:09:48 +0530

    all files

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../styles/hero.css'
+
+function Hero() {
+  return (
+    <>
+    <div className="container-fluid hero">
+          <div className="row">
+            <div className="col-12 col-sm-12 col-md-6">
+                <div className="hero-text p-lg-5 p-md-3 p-sm-2">
+                    <h1 className='display-1 hero-heading'>Don't miss this amazing deal!</h1>
+                    <p className='fs-5 text-secondary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, molestiae eligendi hic nobis aliquam quo consequuntur quis ex laborum ab esse sunt pariatur dolorem excepturi accusamus iusto fuga culpa iure.</p>
+
+                    <div className="btns d-flex gap-2 flex-wrap">
+                    <button className="btn-outline">LEARN MORE</button>
+                    <button className="btn-solid">BUY NOW</button>
+
+                    </div>
+                </div>  
+            </div>
+            <div className="col-12 col-sm-12 col-md-6">
+                <div className="hero-image">
+                    <img src="http://bloxthemes.net/landing-pages/uploads/images/blox_page_builder/headphones.png" alt="hero-image" className='w-100 mt-4' />
+                </div>
+            </div>
+          </div>
+    </div>
+    </>
+  )
+}
+
+export default Hero
